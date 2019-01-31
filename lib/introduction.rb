95910953d0d5b73159c33ea38230fc27da4251eb
Lisa Scott <lrscot@aol.com>
2019-01-31 02:06:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,15 @@
 def introduction
 end
-def
-  introduction(name)
-  end
-  def introduction(name)
+def introduction(name)
+end
+def introduction(name)
     puts "Hi, my name is #{name}."
-  end
-  def introduction_with_language(name, language)
-    puts `"Hi, my name is #{name} and I am learning to program in #{language}."
-  end
+end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
